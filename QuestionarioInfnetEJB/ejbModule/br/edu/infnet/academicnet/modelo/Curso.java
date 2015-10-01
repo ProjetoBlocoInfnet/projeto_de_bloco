d35a8a9eee6d53c95462420a21f78073752dcfb4
Emmanuel Aranha <emmanuelaranha@gmail.com>
2015-09-30 21:55:48 -0300

    ajuste na classe curso

--- QuestionarioInfnetEJB/ejbModule/br/edu/infnet/academicnet/modelo/Curso.java
+++ QuestionarioInfnetEJB/ejbModule/br/edu/infnet/academicnet/modelo/Curso.java
@@ -4,6 +4,8 @@ public class Curso {
 
 	private int idCurso;
 
+	private String nome;
+	
 	public int getIdCurso() {
 		return idCurso;
 	}
@@ -12,4 +14,39 @@ public class Curso {
 		this.idCurso = idCurso;
 	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + idCurso;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Curso other = (Curso) obj;
+		if (idCurso != other.idCurso)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
 }
