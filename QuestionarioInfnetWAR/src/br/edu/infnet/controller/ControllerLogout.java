0c82a20269b0106ed2eae49fd6094b5e881f8e02
Elom Waizmam <waizmam.rj@gmail.com>
2015-10-27 15:26:09 -0200

    Login

---
+++ QuestionarioInfnetWAR/src/br/edu/infnet/controller/ControllerLogout.java
@@ -0,0 +1,44 @@
+package br.edu.infnet.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ControllerLogout
+ */
+@WebServlet("/ControllerLogout")
+public class ControllerLogout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ControllerLogout() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+        session.removeAttribute("pessoa");
+        request.setAttribute("logout", "Usuário deslogado com sucesso!");
+        request.getRequestDispatcher("login.jsp").forward(request, response);
+	}
+
+}
