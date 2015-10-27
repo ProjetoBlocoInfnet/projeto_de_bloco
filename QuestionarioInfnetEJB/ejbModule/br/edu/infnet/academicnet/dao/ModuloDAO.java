beca94f6742e7e118e92b5d5cab22f4689587952
Emmanuel Aranha <emmanuelaranha@gmail.com>
2015-10-27 18:18:58 -0200

    Dao modulo pronto para testes

--- QuestionarioInfnetEJB/ejbModule/br/edu/infnet/academicnet/dao/ModuloDAO.java
+++ QuestionarioInfnetEJB/ejbModule/br/edu/infnet/academicnet/dao/ModuloDAO.java
@@ -1,8 +1,13 @@
 package br.edu.infnet.academicnet.dao;
 
+import java.util.List;
+
+import br.edu.infnet.academicnet.modelo.Avaliacao;
 import br.edu.infnet.academicnet.modelo.Modulo;
 
 public interface ModuloDAO extends GenericoDAO<Modulo>
 {
-	
+	public List<Modulo> obterPorNome(String nome);
+
+	//public boolean excluirModulo(long idAvaliacao, long idQuestao);
 }
