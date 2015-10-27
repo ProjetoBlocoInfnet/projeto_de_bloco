e274afd1cd120ad7a29389c52d98d48abd67b098
Emmanuel Aranha <emmanuelaranha@gmail.com>
2015-10-27 19:33:35 -0200

    Peguei as classes pessoa dao
    Peguei do ultimo submit da branch codificacaoTelas

---
+++ QuestionarioInfnetEJB/ejbModule/br/edu/infnet/academicnet/dao/PessoaDAO.java
@@ -0,0 +1,21 @@
+package br.edu.infnet.academicnet.dao;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+import br.edu.infnet.academicnet.modelo.Pessoa;
+import br.edu.infnet.academicnet.modelo.Usuario;
+
+@Local
+public interface PessoaDAO extends GenericoDAO<Pessoa>{
+	
+	public List<Pessoa> consultarPorNomeDaPessoa(String nome);
+	
+	public List<Pessoa> listarAtivas();
+	
+	public Pessoa login(String login, String senha);
+	
+	public Pessoa obterPorIdUsuario(Usuario usuario);
+
+}
---
+++ QuestionarioInfnetEJB/ejbModule/br/edu/infnet/academicnet/dao/PessoaDAOImpl.java
@@ -0,0 +1,110 @@
+package br.edu.infnet.academicnet.dao;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import br.edu.infnet.academicnet.enumerators.Status;
+import br.edu.infnet.academicnet.modelo.Pessoa;
+import br.edu.infnet.academicnet.modelo.Usuario;
+
+@Stateless
+public class PessoaDAOImpl implements PessoaDAO{
+	
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Override
+	public boolean incluir(Pessoa objeto) {
+		try {			
+			manager.persist(objeto);	
+			manager.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+
+	@Override
+	public boolean alterar(Pessoa objeto) {
+		try {			
+			manager.merge(objeto);
+			manager.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+
+	@Override
+	public boolean excluir(long id) {
+		Pessoa pessoaBanco = manager.find(Pessoa.class, id);		
+		try {
+			//manager.remove(PessoaBanco);
+			pessoaBanco.setStatus(Status.INATIVO);
+			manager.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return true;
+	}
+	
+	
+
+	@Override
+	public Pessoa obter(long id) {		
+		TypedQuery<Pessoa> query = manager.createQuery("select p from Pessoa p where p.matricula=:pId ", Pessoa.class);
+		query.setParameter("pId", id);
+		return query.getSingleResult();
+	}
+	
+	@Override
+	public Pessoa obterPorIdUsuario(Usuario usuario) {		
+		TypedQuery<Pessoa> query = manager.createQuery("select p from Pessoa p where p.usuario=:uId ", Pessoa.class);
+		query.setParameter("uId", usuario);
+		return query.getSingleResult();
+	}
+	
+	@Override
+	public List<Pessoa> consultarPorNomeDaPessoa(String nome) {		
+		TypedQuery<Pessoa> query = manager.createQuery("select p from Pessoa p where p.nome like :pNome order by p.matricula", Pessoa.class);
+		query.setParameter("pNome", "%"+nome+"%" );
+		return query.getResultList();
+		 
+	}
+	@Override
+	public List<Pessoa> listarAtivas() {		
+		TypedQuery<Pessoa> query = manager.createQuery("select p from Pessoa p where p.status=:pAtivo order by p.matricula", Pessoa.class);
+		query.setParameter("pAtivo", Status.ATIVO );
+		return query.getResultList();		 
+	}
+
+	@Override
+	public List<Pessoa> listar() {
+		TypedQuery<Pessoa> query = manager.createQuery("select p from Pessoa p order by p.matricula", Pessoa.class);
+		return query.getResultList();
+	}
+
+	@Override
+	public Pessoa login(String login, String senha) {
+		TypedQuery<Usuario> query = manager.createQuery("select u from Usuario u where u.login=:uLogin and u.senha=:uSenha ", Usuario.class);
+		query.setParameter("uLogin", login);
+		query.setParameter("uSenha", senha);
+		Pessoa pessoa = null;
+		try {
+			Usuario usuario = (Usuario) query.getSingleResult();
+			pessoa =  obterPorIdUsuario(usuario);
+				
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return pessoa;
+		
+	}
+	
+
+}
